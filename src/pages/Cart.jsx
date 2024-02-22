2fc511b0e7d6c3298695d878306670c39214cfe4
Vishn702530 <vishnuharidas5399@gmail.com>
2024-02-22 20:05:12 +0530

    ecart

---
+++ src/pages/Cart.jsx
@@ -0,0 +1,94 @@
+import React, { useEffect, useState } from 'react'
+import {useSelector,useDispatch} from 'react-redux'
+import { Link, useNavigate } from 'react-router-dom'
+import {Button} from 'react-bootstrap'
+import { emptyCart, removeFromCart } from '../redux/slice/cartSlice'
+
+
+
+
+function Cart() {
+  const cartArray = useSelector(state => state.cartReducer)
+  const dispatch=useDispatch()
+  const navigate= useNavigate()
+  const[total,setTotal]=useState(0)
+
+  const getCartTotal =()=>{
+    if(cartArray.length>0){
+      setTotal(cartArray.map(item=>item.price).reduce((p1,p2)=>p1+p2))
+    }else{
+      setTotal(0)
+    }
+  }
+
+  useEffect(()=>{
+    getCartTotal()
+  },[cartArray])
+
+  const handleCart=()=>{
+    dispatch(emptyCart())
+    alert("order placed successfully")
+    navigate('/')
+  }
+  return (
+    <div style={{marginTop:"100px"}}>
+      Your cart
+      {
+        cartArray.length>0?
+          
+<div className="row">
+<div className="col-lg-8">
+  <table className="table shadow rounded">
+    <thead>
+      <tr>
+        <th>#</th>
+        <th>product</th>
+        <th>price</th>
+        <th>action</th>
+      </tr>
+    </thead>
+    <tbody>
+      {
+        cartArray.map((product,index)=>(
+          <tr key={index}>
+            <td>{index+1}</td>
+            <td>{product.title}</td>
+            <td><img src={product.thumbnail} alt='' width={'110px'} height={'100px'} /></td>
+            <td> <Button onClick={()=>dispatch(removeFromCart(product.id))} variant="outline" className='btn ' ><i class="fa-solid fa-trash" style={{color: '#d22014'}}></i></Button></td>
+          </tr>
+        ))
+      }
+    </tbody>
+  </table>
+</div>
+
+<div className="col-lg-3">
+  <div className="border mt-3 rounded shadow p-2 w-100">
+    <h1 className='text-primary p-2'>Cart Summary</h1>
+    <h4>Total Products <span>{cartArray.length}</span></h4>
+    <h4>Total: <span className="text-danger fw-bolder fs-2">${total}</span></h4>
+    <div className="d-grid">
+      <button onClick={handleCart} className="btn btn-success mt-5 rounded">Check Out</button>
+    </div>
+  </div>
+</div>
+
+
+</div>
+
+
+
+        :<div style={{height:'100vh'}} className='w-100 d-flex dlex-column align-items-center justify-content-center'>
+  
+  <img height={'500px'} src="https://farmfreshshop.com/public/img/empty-cart1.gif" alt="hhh" />
+  <Link className='btn btn-warning rounded' style={{textDecoration:'none'}} to={'/'}>Back to Home</Link>
+</div>
+      }
+
+
+
+      </div>
+  )
+}
+
+export default Cart
